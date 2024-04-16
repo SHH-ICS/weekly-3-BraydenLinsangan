50ff707e5711fbd453972c2db486fd93dba32053
BraydenLinsangan <159149939+BraydenLinsangan@users.noreply.github.com>
2024-04-16 13:28:37 +0000

    please work

--- assignment.py
+++ assignment.py
@@ -1,5 +1,6 @@
 import time
-print("Welcome to gaymers' Pizza Parlor!")
+
+print("Welcome to Joseph Manuel Davuis' Pizza Parlor!")
 time.sleep(1)
 print("What pizza size would you like, we serve L or XL")
 PizzaSize = input()
@@ -9,25 +10,96 @@ if PizzaSize != "L" and PizzaSize != "XL":
 elif PizzaSize == "L":
   print("How many toppings?")
   Price = 6
-elif PizzaSize == "XL":
+  Toppings = int(input())
+  if Toppings < 1:
+    print("We only serve pizzas with 1-4 toppings.")
+  if Toppings > 4:
+    print("We only serve pizzas with 1-4 toppings.")
+  if Toppings == 1:
+    Price = (Price+1)
+    Subtotal = Price
+    Tax = (Subtotal*0.13)
+    Total = (Subtotal+Tax)
+    print("Here is the cost for the pizza.")
+    time.sleep(1)
+    print(Subtotal)
+    time.sleep(0.75)
+    print(Total)
+  elif Toppings == 2:
+    Price = (Price+1.75)
+    Subtotal = Price
+    Tax = (Subtotal*0.13)
+    Total = (Subtotal+Tax)
+    print("Here is the cost for the pizza.")
+    time.sleep(1)
+    print(Subtotal)
+    time.sleep(0.75)
+    print(Total)
+  elif Toppings == 3:
+    Price = (Price+2.50)
+    Subtotal = Price
+    Tax = (Subtotal*0.13)
+    Total = (Subtotal+Tax)
+    print("Here is the cost for the pizza.")
+    time.sleep(1)
+    print(Subtotal)
+    time.sleep(0.75)
+    print(Total)
+  elif Toppings == 4:
+    Price = (Price+3.35)
+    Subtotal = Price
+    Tax = (Subtotal*0.13)
+    Total = (Subtotal+Tax)
+    print("Here is the cost for the pizza.")
+    time.sleep(1)
+    print(Subtotal)
+    time.sleep(0.75)
+    print(Total)
+if PizzaSize == "XL":
   print("How many toppings?")
   Price = 10
-Toppings = input()
-if Toppings <= "1" and Toppings >= "5":
-  print("We only serve pizzas with 1-4 toppings.")
-if Toppings == "1":
-  Price = (Price+1)
-elif Toppings == "2":
-  Price = (Price+1.75)
-elif Toppings == "3":
-  Price = (Price+2.50)
-elif Toppings == "4":
-  Price = (Price+3.35)
-Subtotal = Price
-Tax = (Subtotal*0.13)
-Total = (Subtotal+Tax)
-print("Here is the cost for the pizza.")
-time.sleep(1)
-print(Subtotal)
-time.sleep(0.75)
-print(Total)
+  Toppings = int(input())
+  if Toppings < 1:
+    print("We only serve pizzas with 1-4 toppings.")
+  if Toppings > 4:
+    print("We only serve pizzas with 1-4 toppings.")
+  if Toppings == 1:
+    Price = (Price+1)
+    Subtotal = Price
+    Tax = (Subtotal*0.13)
+    Total = (Subtotal+Tax)
+    print("Here is the cost for the pizza.")
+    time.sleep(1)
+    print(Subtotal)
+    time.sleep(0.75)
+    print(Total)
+  elif Toppings == 2:
+    Price = (Price+1.75)
+    Subtotal = Price
+    Tax = (Subtotal*0.13)
+    Total = (Subtotal+Tax)
+    print("Here is the cost for the pizza.")
+    time.sleep(1)
+    print(Subtotal)
+    time.sleep(0.75)
+    print(Total)
+  elif Toppings == 3:
+    Price = (Price+2.50)
+    Subtotal = Price
+    Tax = (Subtotal*0.13)
+    Total = (Subtotal+Tax)
+    print("Here is the cost for the pizza.")
+    time.sleep(1)
+    print(Subtotal)
+    time.sleep(0.75)
+    print(Total)
+  elif Toppings == 4:
+    Price = (Price+3.35)
+    Subtotal = Price
+    Tax = (Subtotal*0.13)
+    Total = (Subtotal+Tax)
+    print("Here is the cost for the pizza.")
+    time.sleep(1)
+    print(Subtotal)
+    time.sleep(0.75)
+    print(Total)
